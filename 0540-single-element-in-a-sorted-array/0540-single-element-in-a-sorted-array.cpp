9120d11701d1e739f97b57785b76771a5a6b656e
Shantanu01B <146628302+Shantanu01B@users.noreply.github.com>
2026-01-31 23:15:10 +0530

    Time: 0 ms (100%), Space: 26.2 MB (67.49%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -11,14 +11,14 @@ public:
         while(start<=end){
             int mid =start +(end-start)/2;
 
-            if(mid ==0 && A[0] !=A[1]){
+            if(mid ==0 && A[0] !=A[1]){ //if mid is first element
                 return A[mid];
             }
-            if(mid == n-1 && A[n-1]!=A[n-2]){
+            if(mid == n-1 && A[n-1]!=A[n-2]){ // if mid is last element
                 return A[mid];
             }
 
-            if( A[mid-1] != A[mid] && A[mid]!=A[mid+1]){
+            if( A[mid-1] != A[mid] && A[mid]!=A[mid+1]){ 
                 return A[mid];
             }
 
